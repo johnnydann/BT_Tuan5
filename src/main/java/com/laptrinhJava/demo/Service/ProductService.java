ac6a82a0bb900303fd4441215853913b1d89ebc5
johnnydann <johnnydang2713@gmail.com>
2024-06-27 18:05:26 +0700

    fix search

--- src/main/java/com/laptrinhJava/demo/Service/ProductService.java
+++ src/main/java/com/laptrinhJava/demo/Service/ProductService.java
@@ -61,7 +61,19 @@ public class ProductService {
             throw new IllegalArgumentException("Not enough quantity in stock for product: " + product.getName());
         }
     }
-    public List<Product> searchProductsByName(String keyword) {
-        return productRepository.findByNameContainingIgnoreCase(keyword);
+    public List<Product> searchProducts(String keyword) {
+        return productRepository.findByKeyword(keyword);
+    }
+
+    public List<Product> searchProductsByKeywordAndCategory(String keyword, Long categoryId) {
+        if (categoryId != null) {
+            return productRepository.findByKeywordAndCategory(keyword, categoryId);
+        } else {
+            return productRepository.findByKeyword(keyword);
+        }
+    }
+
+    public List<Product> searchProductsByKeywordAndCategoryName(String keyword, String categoryName) {
+        return productRepository.findByKeywordAndCategoryName(keyword, categoryName);
     }
 }
--- src/main/java/com/laptrinhJava/demo/repository/ProductRepository.java
+++ src/main/java/com/laptrinhJava/demo/repository/ProductRepository.java
@@ -2,12 +2,21 @@ package com.laptrinhJava.demo.repository;
 
 import com.laptrinhJava.demo.Model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findByNameContainingIgnoreCase(String keyword);
+    @Query("SELECT p FROM Product p WHERE p.name LIKE %:keyword% OR p.description LIKE %:keyword%")
+    List<Product> findByKeyword(@Param("keyword") String keyword);
+
+    @Query("SELECT p FROM Product p WHERE (p.name LIKE %:keyword% OR p.description LIKE %:keyword%) AND p.category.id = :categoryId")
+    List<Product> findByKeywordAndCategory(@Param("keyword") String keyword, @Param("categoryId") Long categoryId);
+
+    @Query("SELECT p FROM Product p WHERE (p.name LIKE %:keyword% OR p.description LIKE %:keyword%) AND p.category.name LIKE %:categoryName%")
+    List<Product> findByKeywordAndCategoryName(@Param("keyword") String keyword, @Param("categoryName") String categoryName);
 }
 
